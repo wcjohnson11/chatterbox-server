f44d5a62c36c18e9a6f52ddd81c3a5e405b7bbb4
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-23 16:40:54 -0700

    Have chat app talking to node server, delivering and receiving msg's

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,15 +5,14 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
-var handleRequest = function(request, response) {
+module.exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
+  // console.log("Serving request type " + request.method + " for url " + request.url);
   var statusCode = 200;
 
   /* Without this line, this server wouldn't work. See the note
@@ -29,7 +28,23 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  if(request.url === '/favicon.ico') {
+    response.end("no ico");
+  }
+  if(request.url === "/1/classes/messages") {
+    if (request.method === "GET") {
+      // response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end(JSON.stringify(testResponse));
+    } else if (request.method === "POST") {
+        request.addListener('data', function(chunk) {
+          console.log('Chunk here');
+          console.log(chunk.toString());
+        });
+    }
+  } else {
+    response.end("Hello, World!");
+  }
+  response.end("404 Page Here");
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -43,3 +58,12 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+var testResponse = {
+  results: [{username: 'nelson',
+          roomname: 'hr',
+              text: 'yo',
+         createdAt: 'now'}]
+};
+
+
